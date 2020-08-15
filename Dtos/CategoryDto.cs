a7d767b6c26650be98dfbf55fe92698bb6e5c359
Andrew Robilliard <robilliardnz@gmail.com>
2020-08-15 16:03:28 +1200

    Add comments to Category DTO

--- Dtos/CategoryDto.cs
+++ Dtos/CategoryDto.cs
@@ -2,9 +2,19 @@ using System;
 
 namespace CategoriseApi.Dtos
 {
+  /// <summary>
+  /// Data transfer unit for Category.
+  /// </summary>
   public class CategoryDto
   {
+    /// <summary>
+    /// Gets or sets the category ID.
+    /// </summary>
     public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the category name.
+    /// </summary>
     public string CategoryName { get; set; }
   }
 }
